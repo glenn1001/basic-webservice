26cf46598260be9fe9a72c352c23646ed36bae92
Glenn Blom <glennblom@gmail.com>
2014-06-17 01:19:48 +0200

    * Change database settings

--- app/config/database.php
+++ app/config/database.php
@@ -55,12 +55,12 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
+			'database'  => 'laravel_webservice',
+			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
+			'prefix'    => 'ws_',
 		),
 
 		'pgsql' => array(
